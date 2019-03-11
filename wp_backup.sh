1bd9fec72efc8b79fb2660b26f491fc166c102bc
root <root@menta.me>
2019-03-12 01:35:44 +0900

    wordpress backup

--- wp_backup.sh
+++ wp_backup.sh
@@ -13,7 +13,7 @@ dirpath='/root/backup/menta_backup/'
 filename=`date +%y%m%d`
 
 # mysqldumpの実行(圧縮ファイルで保存する)
-mysqldump --opt --all-databases --events --default-character-set=binary -u root --password=Sakasitasakasu3859! | gzip > $dirpath/$filename.sql.gz
+mysqldump --opt --all-databases --events --default-character-set=binary -u root --password=********** | gzip > $dirpath/$filename.sql.gz
 
 # 古いバックアップファイルを削除
 oldfile=`date --date "$period days ago" +%y%m%d`
