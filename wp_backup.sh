310bcd0c6e85c4acd1bd70eaf05e3ce03fddca0d
root <root@menta.me>
2019-04-09 22:07:58 +0900

    コンフリクト解消

--- wp_backup.sh
+++ wp_backup.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-DIR=/root/mount-s3/backup
+DIR=/root//backup
 
 DATE=`date +%Y%m%d`
 OLD_DATE=`date +%Y%m%d --date "5 days ago"`
@@ -8,6 +8,6 @@ OLD_DATE=`date +%Y%m%d --date "5 days ago"`
 DUMP=$DIR/mysqldump.$DATE.sql
 OLD_DUMP=$DIR/mysqldump.$OLD_DATE.sql
 
-mysqldump -u root -pxxxx --all-databases > $DUMP
+mysqldump -u root -pSakasitasakasu3859! --all-databases > $DUMP
 
 rm -f $OLD_DUMP
